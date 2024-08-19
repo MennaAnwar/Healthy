6cf4e1198ad2b50e5de4d00a2f11f19813c68c2a
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-08-19 10:45:15 +0200

    creating appointment

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+import { getPatient } from "@/lib/actions/patient.actions";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <div className="logo mb-12 flex items-center">
+            <Image
+              src="/assets/icons/logo.png"
+              height={50}
+              width={50}
+              alt="patient"
+              className="me-4"
+            />
+            <h1 className="font-extrabold text-2xl text-blue-100">HEALTHY</h1>
+          </div>
+
+          <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create"
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+};
+
+export default Appointment;
--- lib/validation.ts
+++ lib/validation.ts
@@ -51,3 +51,44 @@ export const PatientFormValidation = z.object({
   Identification_number: z.string().optional(),
   identificationDocument: z.custom<File[]>().optional(),
 });
+
+export function getAppointmentSchema(type: string) {
+  switch (type) {
+    case "create":
+      return CreateAppointmentSchema;
+    case "cancel":
+      return CancelAppointmentSchema;
+    default:
+      return ScheduleAppointmentSchema;
+  }
+}
+
+export const CreateAppointmentSchema = z.object({
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  schedule: z.coerce.date(),
+  reason: z
+    .string()
+    .min(2, "Reason must be at least 2 characters")
+    .max(500, "Reason must be at most 500 characters"),
+  note: z.string().optional(),
+  cancellationReason: z.string().optional(),
+});
+
+export const ScheduleAppointmentSchema = z.object({
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  schedule: z.coerce.date(),
+  reason: z.string().optional(),
+  note: z.string().optional(),
+  cancellationReason: z.string().optional(),
+});
+
+export const CancelAppointmentSchema = z.object({
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  schedule: z.coerce.date(),
+  reason: z.string().optional(),
+  note: z.string().optional(),
+  cancellationReason: z
+    .string()
+    .min(2, "Reason must be at least 2 characters")
+    .max(500, "Reason must be at most 500 characters"),
+});
